6c2fb6268003aeda8793b080ad8631a40a59ddcc
Prajwal Mohiture <prajwalmohiture8@gmail.com>
2026-09-10 19:05:30 +0530

    setbasics are added

---
+++ Chapter-5/assignment.py
@@ -0,0 +1,11 @@
+# create a dictionary named marks to store marks of 3 subjects.
+# add the subject one by one and print the final dictionary
+ 
+marks={}
+
+marks["maths"]=99
+marks["chemistry"]=99
+marks["computer science"]=91
+print(marks)
+
+# 
